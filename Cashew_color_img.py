07f0c5dca98e46bd6f09040dedca5ffca2f0e3a2
Raj Dalsaniya <rajdalsaniya1995@gmail.com>
2022-02-08 18:12:55 +0530

    Add files via upload

--- Cashew_color_img.py
+++ Cashew_color_img.py
@@ -3,11 +3,13 @@ import numpy as np
 
 frame = cv2.imread("pic\IMG-20220205-WA0009.jpg")
 
-# converting  BGR to HSV Frame
-hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+frame_blur = cv2.GaussianBlur(frame, (7, 7), 0)
 
-cashew_hsv_lower = np.array([7, 100, 100], np.uint8)
-cashew_hsv_upper = np.array([30, 255, 255], np.uint8)
+# converting  BGR to HSV Frame
+hsv = cv2.cvtColor(frame_blur, cv2.COLOR_BGR2HSV)
+
+cashew_hsv_lower = np.array([6, 65, 128], np.uint8)
+cashew_hsv_upper = np.array([54, 255, 255], np.uint8)
 
 # finding the range of cashew_hsv color in the image
 cashew_hsv = cv2.inRange(hsv, cashew_hsv_lower, cashew_hsv_upper)
@@ -22,12 +24,15 @@ res = cv2.bitwise_and(frame, frame, mask = cashew_hsv)
 contours, hierarchy = cv2.findContours(cashew_hsv, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 for i, contour in enumerate(contours) :
     area = cv2.contourArea(contour)
+
     if hierarchy[0, i, 3] == -1 :
-        if area > 2400 :
-            x, y, w, h = cv2.boundingRect(contour)
+        x, y, w, h = cv2.boundingRect(contour)
+        if area > 2500 and h > 42:
+            print(x, y, w, h)
             frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.putText(frame, "cashew", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0))
 
 cv2.imshow("Color", frame)
+cv2.imshow("Cashew", hsv)
 if cv2.waitKey(0) & 0xFF == ord('q'):
     cv2.destroyAllWindows()
